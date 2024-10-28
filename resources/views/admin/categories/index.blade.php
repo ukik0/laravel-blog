f23eba3e763951cb85a9e65b985a531cdb8fbe35
Dima <ctaricc@mail.ru>
2024-10-28 09:25:10 +0300

    feat: show categories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -25,7 +25,33 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                        <a href="{{route('admin.category.create')}}" class="btn w-auto btn-block btn-primary">Добавить</a>
+                    <a href="{{route('admin.category.create')}}" class="btn w-auto btn-block btn-primary mb-4">Добавить</a>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Название</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <td>{{$category->id}}</td>
+                                            <td>{{$category->title}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
                 </div>
                 <!-- /.row -->
 
